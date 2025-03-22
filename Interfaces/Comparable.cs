271ae6e49455d0c0073f652b138d3594c81e802a
Damian Pazos <damian_ariel_pazos>
2025-03-21 23:54:42 -0300

    Agregar archivos de proyecto.

---
+++ Interfaces/Comparable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodologiasDeProgramacion.Interfaces
+{
+    // Ejercicio N1 - Practica N1
+    internal interface Comparable
+    {
+        bool sosIgual(Comparable c);
+        bool sosMenor(Comparable c);
+        bool sosMayor(Comparable c);
+    }
+}
