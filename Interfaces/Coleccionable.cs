3e29c91817f9997ee2368d47e40ea715ac641ca0
Damian Pazos <damian_ariel_pazos>
2025-03-25 16:22:44 -0300

    feat: Ejercicio N°3 - Practica N°1

---
+++ Interfaces/Coleccionable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodologiasDeProgramacion.Interfaces
+{
+    // Ejercicio N°3 - Practica N°1
+    public interface Coleccionable
+    {
+        int cuantos();
+        Comparable minimo();
+        Comparable maximo();
+        void agregar(Comparable c);
+        void contiene(Comparable c);
+    }
+}
