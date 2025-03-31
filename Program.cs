bbec021dcb500c94d24f4429fa7bccf515207eaa
Damian Pazos <damian_ariel_pazos>
2025-03-30 23:09:39 -0300

    feat: Ejercicio N°5 - Practica N°1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Ejercicio N°5 - Practica N°1
+
+using MetodologiasDeProgramacion.Clases;
+using MetodologiasDeProgramacion.Interfaces;
+
+namespace MetodologiasDeProgramacion;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // codigo programa
+    }
+
+    public void llenar(Coleccionable c)
+    {
+        Random random = new Random();
+        for (int contador = 1; contador < 21; contador++)
+        {
+            Comparable numero = new Numero(random.Next(1, 100000));
+            c.agregar(numero);
+        }
+    }
+}
+
+
