69d3778f5f127a3f012aad7a332adca81c49bc53
Damian Pazos <damian_ariel_pazos>
2025-04-02 18:29:50 -0300

    feat: Ejercicio N°12 - Practica N°1

---
+++ Clases/Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodologiasDeProgramacion.Clases
+{
+    // Ejercicio N°12 - Practica N°1
+    public class Alumno : Persona
+    {
+        private int legajo;
+        private float promedio;
+
+        public Alumno(string nombre, int dni, int legajo, float promedio) : base(nombre, dni)
+        {
+            this.legajo = legajo;
+            this.promedio = promedio;
+        }
+
+        public int getLegajo()
+        {
+            return legajo;
+        }
+
+        public float getPromedio()
+        {
+            return promedio;
+        }
+    }
+}
